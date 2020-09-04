60afe6ccb84ef3cd4c910bda872692b9e89a5eab
LIN <yi19930716@gmail.com>
2020-09-04 10:51:07 +0800

    feat: add facebook login

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const home = require('./models/home.js')
 const users = require('./models/users.js')
 const todos = require('./models/todos.js')
+const auth = require('./models/auth.js')
 const { authenticator } = require('../middleware/auth.js')
 
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/todos', authenticator, todos)
 router.use('/', authenticator, home)
 
